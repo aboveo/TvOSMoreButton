8164908e8fe83f878a02060db1987b61aeed1efa
NE <naren.edpuganti@aboveo.com>
2017-10-02 22:46:28 -0400

    New code 1.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,41 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+`TvOSMoreButton` adheres to [Semantic Versioning](http://semver.org/).
+
+#### 1.x Releases
+- `1.0.x` Releases - [1.0.0](#100) | [1.0.1](#101) | [1.0.2](#102)
+
+## [1.0.2](https://github.com/cgoldsby/TvOSMoreButton/releases/tag/1.0.2)
+Released on 2017-04-233.
+#### Support More Localizations: 🇲🇽, 🇯🇵, 🇳🇱, 🇳🇴, 🇷🇺
+
+No API changes.
+
+#### Added
+
+The `TvOSMoreButton` is now localized in:
+
+Español Mexicano 🇲🇽   
+Japanese 🇯🇵  
+Dutch 🇳🇱  
+Norwegian 🇳🇴
+Russian 🇷🇺
+
+## [1.0.1](https://github.com/cgoldsby/TvOSMoreButton/releases/tag/1.0.1)
+Released on 2017-03-25.
+#### Support More Localizations: 🇩🇪, 🇫🇷, 🇪🇸, 🇮🇹
+
+No API changes.
+
+#### Added
+
+The `TvOSMoreButton` is now localized in:
+
+Español 🇩🇪   
+French 🇫🇷  
+Deutsch 🇪🇸  
+Italian 🇮🇹
+
+## [1.0.0](https://github.com/cgoldsby/TvOSMoreButton/releases/tag/1.0.0)
+Released on 2017-03-09.
+#### Initial Release 🎉
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//
+// Copyright (c) 2016-2017 Chris Goldsby
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import UIKit
+import TvOSMoreButton
+import TvOSTextViewer
+
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var tvOSMoreButton: TvOSMoreButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpUI()
+    }
+
+    // MARK: - Private
+    
+    private func setUpUI() {
+        tvOSMoreButton.text = "It is the year 1985. Marty McFly, a mild-mannered high school student, stopped by Dr. Emmett L. Brown's laboratory to play around with an amplifier. Then he receives a message from Doc that he needs help from him for Doc's latest invention, a time machine made out of a DeLorean sports car that can travel through time instantaneously when it reaches a speed velocity of 88 MPH. Then, Doc was gunned down by Libyan Nationalists, Marty makes an effort to escape from the Lybians by using the time machine. Then Marty accidentally warps himself into 1955. Where he meets both of his parents when they were teenagers, then Marty unintentionally interrupts his parent's first meeting together, he then finds a younger version of Doc and together they try to find a way to get Marty's parents-to-be back together, and to get Marty back to 1985."
+        tvOSMoreButton.buttonWasPressed = moreButtonWasPressed
+    }
+    
+    private func moreButtonWasPressed(text: String?) {
+        let viewController = TvOSTextViewerViewController()
+        viewController.text = text
+        viewController.textColor = .white
+        viewController.textEdgeInsets = UIEdgeInsets(top: 100, left: 250, bottom: 100, right: 250)
+        present(viewController, animated: true)
+    }
+}
